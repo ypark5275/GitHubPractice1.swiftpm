1a3aaa74972af55f90e5507921a8eefc79407a68
Aneena Ginson <aginson7135@stu.d214.org>
2024-04-10 14:51:39 -0500

    added numner2

--- ContentView.swift
+++ ContentView.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State var number1 = 7
+    @State var number2 = 0
     var body: some View {
         VStack {
             Image(systemName: "globe")
